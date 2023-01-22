ce2efcf8a534267e95d3e58a785c5a90b97f1456
Ilya Pcn <pchelinilya13@gmail.com>
2023-01-22 15:11:40 +1100

    feat: add main func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function toReadable (number) {
-  
+
+module.exports = function toReadable (num) {
+    const lowNums = ['',' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine',' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen',' seventeen', ' eighteen', ' nineteen'];
+    const hightNums = ['','',' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty',' ninety']; 
+    const hundred = ' hundred';
+
+     if(num === 0){
+        return 'zero'
+    } else if ( num < 20 ){
+        return lowNums[num].substring(1) 
+    } else if ( num < 100) {
+        return hightNums[parseInt(num.toString()[0])].substring(1)  + lowNums[parseInt(num.toString()[1])]
+    } else if(num > 100 && parseInt(num.toString().substring(1)) < 20) {
+        return  lowNums[parseInt(num.toString()[0])].substring(1) + hundred+ lowNums[parseInt(num.toString().substring(1))]
+    } else {
+        return  lowNums[parseInt(num.toString()[0])].substring(1) + hundred + hightNums[parseInt(num.toString()[1])] + lowNums[parseInt(num.toString()[2])]
+    }
 }
+// я уже делал это задание на stage 0, набор декабрь 21го
